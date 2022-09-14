90add83bbe69a104726cd4b134318dc1c3f6a088
admin <marcial.dev@gmail.com>
2022-09-14 16:56:00 -0300

    Implemented Qualification and Services sections

--- js/script.js
+++ js/script.js
@@ -26,3 +26,61 @@ function selectSkill() {
 $skillButtons.forEach((skillItem) => {
    skillItem.addEventListener("click", selectSkill)
 })
+
+/* QUALIFICATION TABS */
+const $tabQualification = document.querySelectorAll(".qualification-button")
+let $tabActive = document.querySelector(".qualification-button.-active")
+
+function selectQualification(item) {
+   item.preventDefault();
+   let $tabClicked = this;
+   $tabActive.classList.remove("-active");
+   $tabClicked.classList.add("-active");
+   $tabActive = $tabClicked;
+
+   let $contentActive = document.querySelector(".qualification-content.-active");
+   $contentActive.classList.remove("-active");
+   let idContent = $tabClicked.getAttribute("href");
+   let $targetContent = document.querySelector(idContent);
+   $targetContent.classList.add("-active");
+}
+
+$tabQualification.forEach((tabItem) => {
+   tabItem.addEventListener("click", selectQualification)
+})
+
+/* SERVICES MODAL */
+const $cardButtons = document.querySelectorAll(".card-button")
+const $serviceModals = document.querySelectorAll(".service-modal")
+const $closeModals = document.querySelectorAll(".modal-close")
+
+let modalService = (modalClick) => {
+   $serviceModals[modalClick].classList.add("-active")
+}
+
+$cardButtons.forEach((cardButton, i) => {
+   cardButton.addEventListener("click", () => {
+      modalService(i)
+   })
+})
+
+$closeModals.forEach((closeModal) => {
+   closeModal.addEventListener("click", () => {
+      $serviceModals.forEach((serviceModal) => {
+         serviceModal.classList.remove("-active")
+      })
+   })
+})
+
+/* PORTFOLIO CAROUSEL */
+const $portfolioCarousel = document.querySelector(".glide.portfolio-carousel")
+
+new Glide($portfolioCarousel, {
+   type: "carousel",
+   gap: 0,
+   startAt: 1,
+   perView: 1,
+   autoplay: 3000,
+   hoverpause: true,
+   animationDuration: 1000
+}).mount()
